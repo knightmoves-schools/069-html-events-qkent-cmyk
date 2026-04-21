5107e40b1604f6e043e04adf6f864cb3b6ef4442
Kent1an <qkent@anamosa.k12.ia.us>
2026-04-21 18:28:06 +0000

    done

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function calculateSquare(){
     let value = document.getElementById('number').value;
     let result = document.getElementById('result');
 
-    //add code here
+    let num = parseFloat(value);
+    if (isNaN(num)) {
+        result.innerHTML = 'Invalid, please enter a number';
+    } else {
+        result.innerHTML = num * num;
+    }
     
 }
+
+document.getElementById('calculate').addEventListener('click', calculateSquare);
